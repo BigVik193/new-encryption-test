5cc26c4a326d78fb6e4359e2826a0ed01564eec1
BigVik193 <76181207+BigVik193@users.noreply.github.com>
2025-04-22 00:51:43 -0500

    Add src/App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>New Encryption Test</h1>
+        <p>
+          Welcome to the encryption test application!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
